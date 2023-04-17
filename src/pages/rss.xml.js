2857aefe9e9971352fffa4358d47c228766141ae
xgyxss <2414923128@qq.com>
2023-04-17 16:29:27 +0800

    update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,8 @@ export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: 'https://yexingshusheng.netlify.app/',
+    // site: 'https://yexingshusheng.netlify.app/',
+    site: 'https://xgyxss.github.io/astro',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
