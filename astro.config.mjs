0af158043d22c08c9dd7d4834d6e4cfc79d092cf
xgyxss <2414923128@qq.com>
2023-04-17 23:55:38 +0800

    解决base设置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import preact from "@astrojs/preact";
 export default defineConfig({
   integrations: [preact()],
   site: 'https://xgyxss.github.io',
-  // base: '/astro',  //npm run dev后，会让生成的访问地址加上/astro，示例：http://localhost:3000/astro
+  base: '/astro',  //npm run dev后，会让生成的访问地址加上/astro，示例：http://localhost:3000/astro
 });
